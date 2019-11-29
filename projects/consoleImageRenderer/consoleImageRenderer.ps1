daf1e93a8aaa59910b2f1bad23306d2b19a6442f
DevAndersen <martinba.private@gmail.com>
2019-11-29 21:12:31 +0100

    Reworked consoleImageRenderer
    The image is now written out one line at a time, rather than one character at a time, significantly speeding up image rendering time.
    Added new parameter set, "FillMode", allowing for different fill-based image resizing.
    The console cursor is now hidden during image rendering.
    Code cleanup.

--- projects/consoleImageRenderer/consoleImageRenderer.ps1
+++ projects/consoleImageRenderer/consoleImageRenderer.ps1
@@ -1,8 +1,15 @@
+#region Parameters
+
 [CmdletBinding(DefaultParameterSetName = "Normal")]
 param(
 	[Parameter(Mandatory, ParameterSetName = "Normal")]
 	[Parameter(Mandatory, ParameterSetName = "Resize")]
+	[Parameter(Mandatory, ParameterSetName = "FillMode")]
 	[String]$Path,
+	
+	[Parameter(Mandatory, ParameterSetName = "FillMode")]
+	[ValidateSet("Stretch", "ProportionalWidth", "ProportionalHeight")]
+	[String]$FillMode,
 	
 	[Parameter(Mandatory, ParameterSetName = "Resize")]
 	[Int]$Width,
@@ -11,41 +18,86 @@ param(
 	[Int]$Height
 )
 
+#endregion
+
+#region Functions
+
+function RenderImage([System.Drawing.Image]$Image)
+{
+	$escape = [Char]0x1B
+	$halfCharString = ([Char]0x2580).ToString()
+
+	[Console]::CursorVisible = $false
+	for ($y = 0; $y -lt $Image.Height; $y += 2)
+	{
+		$pixelString = ""
+		for ($x = 0; $x -lt $Image.Width; $x++)
+		{
+			$f = $Image.GetPixel($x, $y)
+			$pixelString += "$escape[38;2;$($f.R);$($f.G);$($f.b)m"
+			
+			if ($y -lt $Image.Height - 1)
+			{
+				$b = $Image.GetPixel($x, $y + 1)
+				$pixelString += "$escape[48;2;$($b.R);$($b.G);$($b.B)m"
+			}
+			
+			$pixelString += $halfCharString
+		}
+		Write-Host $pixelString
+	}
+	[Console]::CursorVisible = $true
+}
+
+function ResizeImage([System.Drawing.Image]$Image, $NewWidth, $NewHeight)
+{
+	return $img.GetThumbnailImage($NewWidth, $NewHeight, $null, [IntPtr]::Zero)
+}
+
+#endregion
+
+#region Main flow
+
 [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
 
 $absolutePath = Resolve-Path $Path
 $imageFileStream = [System.IO.File]::OpenRead($absolutePath)
 $img = [System.Drawing.Image]::FromStream($imageFileStream, $false, $false)
 
-if ($PSCmdlet.ParameterSetName -eq "Resize")
+switch ($PSCmdlet.ParameterSetName)
 {
-	$img = $img.GetThumbnailImage($Width, $Height, $null, [System.IntPtr]::Zero)
-}
-
-$escape = [Char]0x1B
-$halfCharString = ([Char]0x2580).ToString()
-
-for ($y = 0; $y -lt $img.Height; $y += 2)
-{
-	for ($x = 0; $x -lt $img.Width; $x++)
+	"Resize"
 	{
-		$pixelString = ""
-		
-		$f = $img.GetPixel($x, $y)
-		$pixelString += "$escape[38;2;$($f.R);$($f.G);$($f.b)m"
-		
-		if ($y -lt $img.Height - 1)
+		$img = ResizeImage -Image $img -NewWidth $Width -NewHeight $Height
+	}
+	"FillMode"
+	{
+		switch ($FillMode)
 		{
-			$b = $img.GetPixel($x, $y + 1)
-			$pixelString += "$escape[48;2;$($b.R);$($b.G);$($b.B)m"
+			"Stretch"
+			{
+				$w = [Console]::WindowWidth
+				$h = [Console]::WindowHeight * 2
+			}
+			"ProportionalWidth"
+			{
+				$w = [Console]::WindowWidth
+				$h = ($img.Height / $img.Width) * [Console]::WindowWidth
+			}
+			"ProportionalHeight"
+			{
+				$w = ($img.Height / $img.Width) * [Console]::WindowHeight * 2
+				$h = [Console]::WindowHeight * 2
+			}
 		}
 		
-		$pixelString += $halfCharString
-		
-		Write-Host $pixelString -NoNewline
+		$img = ResizeImage -Image $img -NewWidth $w -NewHeight $h
 	}
-	Write-Host
 }
+
+RenderImage -Image $img 
 
 $img.Dispose()
 $imageFileStream.Dispose()
+
+#endregion
