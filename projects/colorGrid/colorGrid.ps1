96b8a2aa6fbbffdb3662a50b70ddf0ca1ccdd8c8
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2021-01-14 14:20:01 +0100

    Updated project colorGrid, added parameter "AddExtraBlackRow"
    Added a switch parameter, "AddExtraBlackRow", which adds an extra row where the background is explicitly set to black (RGB 0,0,0), using ANSI escape sequences. This is to accomodate how Windows Terminal treats black character backgrounds in relation to the background of the terminal.

--- projects/colorGrid/colorGrid.ps1
+++ projects/colorGrid/colorGrid.ps1
@@ -1,14 +1,30 @@
+param(
+	[Switch]$AddExtraBlackRow
+)
+
 Write-Host
 for ($y = 0; $y -le 15; $y++)
 {
 	for ($x = 0; $x -le 15; $x++)
 	{
 		# Hexadecimalification
-		$sX = [convert]::ToString($x, 16)
-		$sy = [convert]::ToString($y, 16)
+		$hexX = [convert]::ToString($x, 16)
+		$hexY = [convert]::ToString($y, 16)
 		
-		Write-Host " $sX$sY " -ForegroundColor ([System.ConsoleColor]$x) -BackgroundColor ([System.ConsoleColor]$y) -NoNewline
+		Write-Host " $hexX$hexY " -ForegroundColor ([System.ConsoleColor]$x) -BackgroundColor ([System.ConsoleColor]$y) -NoNewline
 	}
 	Write-Host
 }
+
+if ($AddExtraBlackRow)
+{
+	$encodingChar = [char]27
+	for ($z = 0; $z -le 15; $z++)
+	{
+		$hexZ = [convert]::ToString($z, 16)
+		Write-Host "$encodingChar[48;2;0;0;0m $($hexZ)0 $encodingChar[0m" -ForegroundColor ([System.ConsoleColor]$z) -NoNewline
+	}
+	Write-Host
+}
+
 Write-Host
